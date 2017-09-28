54dbd411f621a3200f69f33808d99b9df839f665
raditya <raditya@indovti.com>
2017-09-28 17:46:14 +0700

    readd User bll not commited due to .gitignore

---
+++ FMS.BLL/FMS.BLL.User/UserBLL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.BusinessObject;
+using FMS.Contract;
+using FMS.Contract.BLL;
+using FMS.Contract.Service;
+using FMS.DAL.Services;
+using NLog;
+
+namespace FMS.BLL.User
+{
+    public class UserBLL : IUserBll
+    {
+        private ILogger _logger;
+        private IUserService _userService;
+        
+
+        public UserBLL(IUnitOfWork uow, ILogger logger)
+        {
+            _logger = logger;
+            _userService = new UserService(uow,_logger);
+            
+        }
+
+
+        public List<SysUser> GetAllUsers()
+        {
+            return _userService.GetAllUser();
+        }
+    }
+}
