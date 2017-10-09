f554ccedb6f6aab6a5781fa894828fcaf335d620
Yuniar Prasetyo <tyo@indovti.com>
2017-10-09 10:24:38 +0700

    add mst complaint

--- FMS.Website/Controllers/HomeController.cs
+++ FMS.Website/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace FMS.Website.Controllers
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+            return View("Index");
         }
 
         public ActionResult About()
