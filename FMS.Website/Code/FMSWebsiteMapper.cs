b6662f7de62c3c4c759fb32ae2ea58224baaa5eb
DESKTOP-EFTUD3J\pc <andhika.visible@gmail.com>
2017-10-24 14:33:07 +0700

    resolve conflict

--- FMS.Website/Code/FMSWebsiteMapper.cs
+++ FMS.Website/Code/FMSWebsiteMapper.cs
@@ -135,7 +135,6 @@ namespace FMS.Website.Code
 
             //End Map Master Employee//
 
-<<<<<<< HEAD
             //BEGIN Master Data Vehicle Spect//
             Mapper.CreateMap<VehicleSpectDto, VehicleSpectItem>().IgnoreAllNonExisting()
                .ForMember(dest => dest.Models, opt => opt.MapFrom(src => src.Model))
@@ -154,8 +153,22 @@ namespace FMS.Website.Code
                 .ForMember(dest => dest.Colour, opt => opt.MapFrom(src => src.COLOUR))
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.IMAGE))
                 .ForMember(dest => dest.GroupLevel, opt => opt.MapFrom(src => src.GROUP_LEVEL))
-                .ForMember(dest => dest.FlexPoint, opt => opt.MapFrom(src => src.FLEX_POINT))
-=======
+                .ForMember(dest => dest.FlexPoint, opt => opt.MapFrom(src => src.FLEX_POINT));
+
+            Mapper.CreateMap<VehicleSpectDto, MST_VEHICLE_SPECT>().IgnoreAllNonExisting()
+              .ForMember(dest => dest.MST_VEHICLE_SPECT_ID, opt => opt.MapFrom(src => src.MstVehicleSpectId))
+                .ForMember(dest => dest.MANUFACTURER, opt => opt.MapFrom(src => src.Manufacturer))
+                .ForMember(dest => dest.MODEL, opt => opt.MapFrom(src => src.Model))
+                .ForMember(dest => dest.SERIES, opt => opt.MapFrom(src => src.Series))
+                .ForMember(dest => dest.BODY_TYPE, opt => opt.MapFrom(src => src.BodyType))
+                .ForMember(dest => dest.YEAR, opt => opt.MapFrom(src => src.Year))
+                .ForMember(dest => dest.COLOUR, opt => opt.MapFrom(src => src.Colour))
+                .ForMember(dest => dest.IMAGE, opt => opt.MapFrom(src => src.Image))
+                .ForMember(dest => dest.GROUP_LEVEL, opt => opt.MapFrom(src => src.GroupLevel))
+                .ForMember(dest => dest.FLEX_POINT, opt => opt.MapFrom(src => src.FlexPoint));
+
+            //END Master Data Vehicle Spect//
+
             // Start --- Master Data -> PriceList
             Mapper.CreateMap<PriceListDto, PriceListItem>().IgnoreAllNonExisting()
                 .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate == null ? src.CreatedDate : src.ModifiedDate));
@@ -172,27 +185,12 @@ namespace FMS.Website.Code
                 .ForMember(dest => dest.InstallmenHMS, opt => opt.MapFrom(src => src.INSTALLMEN_HMS))
                 .ForMember(dest => dest.InstallmenEMP, opt => opt.MapFrom(src => src.INSTALLMEN_EMP))
                 .ForMember(dest => dest.Vendor, opt => opt.MapFrom(src => src.VENDOR))
->>>>>>> story/21453-master-data-create-page-master-data-setting
                 .ForMember(dest => dest.ModifiedBy, opt => opt.MapFrom(src => src.MODIFIED_BY))
                 .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.MODIFIED_DATE))
                 .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CREATED_DATE))
                 .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CREATED_BY))
                 .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IS_ACTIVE));
 
-<<<<<<< HEAD
-
-            Mapper.CreateMap<VehicleSpectDto, MST_VEHICLE_SPECT>().IgnoreAllNonExisting()
-              .ForMember(dest => dest.MST_VEHICLE_SPECT_ID, opt => opt.MapFrom(src => src.MstVehicleSpectId))
-                .ForMember(dest => dest.MANUFACTURER, opt => opt.MapFrom(src => src.Manufacturer))
-                .ForMember(dest => dest.MODEL, opt => opt.MapFrom(src => src.Model))
-                .ForMember(dest => dest.SERIES, opt => opt.MapFrom(src => src.Series))
-                .ForMember(dest => dest.BODY_TYPE, opt => opt.MapFrom(src => src.BodyType))
-                .ForMember(dest => dest.YEAR, opt => opt.MapFrom(src => src.Year))
-                .ForMember(dest => dest.COLOUR, opt => opt.MapFrom(src => src.Colour))
-                .ForMember(dest => dest.IMAGE, opt => opt.MapFrom(src => src.Image))
-                .ForMember(dest => dest.GROUP_LEVEL, opt => opt.MapFrom(src => src.GroupLevel))
-                .ForMember(dest => dest.FLEX_POINT, opt => opt.MapFrom(src => src.FlexPoint))
-=======
             Mapper.CreateMap<PriceListDto, MST_PRICELIST>().IgnoreAllNonExisting()
                 .ForMember(dest => dest.MST_PRICELIST_ID, opt => opt.MapFrom(src => src.MstPriceListId))
                 .ForMember(dest => dest.YEAR, opt => opt.MapFrom(src => src.Year))
@@ -238,18 +236,13 @@ namespace FMS.Website.Code
                 .ForMember(dest => dest.FUNCTION_GROUP, opt => opt.MapFrom(src => src.FunctionGroup))
                 .ForMember(dest => dest.FUNCTION_NAME, opt => opt.MapFrom(src => src.FunctionName))
                 .ForMember(dest => dest.FUNCTION_VALUE, opt => opt.MapFrom(src => src.FunctionValue))
->>>>>>> story/21453-master-data-create-page-master-data-setting
                 .ForMember(dest => dest.MODIFIED_BY, opt => opt.MapFrom(src => src.ModifiedBy))
                 .ForMember(dest => dest.MODIFIED_DATE, opt => opt.MapFrom(src => src.ModifiedDate))
                 .ForMember(dest => dest.CREATED_DATE, opt => opt.MapFrom(src => src.CreatedDate))
                 .ForMember(dest => dest.CREATED_BY, opt => opt.MapFrom(src => src.CreatedBy))
                 .ForMember(dest => dest.IS_ACTIVE, opt => opt.MapFrom(src => src.IsActive));
-<<<<<<< HEAD
-            //END Master Data Vehicle Spect//
-=======
 
             // End --- Master Data -> Setting
->>>>>>> story/21453-master-data-create-page-master-data-setting
         }
     }
 }
--- FMS.Website/Global.asax.cs
+++ FMS.Website/Global.asax.cs
@@ -26,12 +26,8 @@ using FMS.BLL.Remark;
 using FMS.BLL.Penalty;
 using FMS.BLL.DocumentType;
 using FMS.BLL.Reason;
-<<<<<<< HEAD
 using FMS.BLL.LocationMapping;
-=======
 using FMS.BLL.Setting;
->>>>>>> story/21453-master-data-create-page-master-data-setting
-
 using FMS.BLL.VehicleSpect;
 namespace FMS.Website
 {
@@ -74,11 +70,9 @@ namespace FMS.Website
             RemarkMapper.Initialize();
             DocumentTypeMapper.Initialize();
             ReasonMapper.Initialize();
-<<<<<<< HEAD
             LocationMapingMapper.Initialize();
-=======
             SettingMapper.Initialize();
->>>>>>> story/21453-master-data-create-page-master-data-setting
+
             // 1. Create a new Simple Injector container
             var container = new Container();
 
@@ -99,12 +93,10 @@ namespace FMS.Website
             container.Register<IRemarkBLL, RemarkBLL>();
             container.Register<IDocumentTypeBLL, DocumentTypeBLL>();
             container.Register<IReasonBLL, ReasonBLL>();
-<<<<<<< HEAD
             container.Register<ILocationMappingBLL, LocationMappingBLL>();
             container.Register<IVehicleSpectBLL , VehicleSpectBLL>();
-=======
             container.Register<ISettingBLL, SettingBLL>();
->>>>>>> story/21453-master-data-create-page-master-data-setting
+
             // 3. Optionally verify the container's configuration.
             container.Verify();
 
