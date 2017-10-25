cf3beba2981bbecc4f4633b0311ccd8f39af2a2d
doni <rusdani@indovto.com>
2017-10-25 14:52:24 +0700

    Create Page Master PenaltyLogic

---
+++ FMS.DAL/Services/PenaltyLogicService.cs
@@ -0,0 +1,39 @@
+﻿using FMS.BusinessObject;
+using FMS.Contract;
+using FMS.Contract.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.DAL.Services
+{
+    public class PenaltyLogicService : IPenaltyLogicService
+    {
+        private IGenericRepository<MST_PENALTY_LOGIC> _penaltyLogicRepository;
+        private IUnitOfWork _uow;
+        public PenaltyLogicService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _penaltyLogicRepository = uow.GetGenericRepository<MST_PENALTY_LOGIC>();
+        }
+
+        public MST_PENALTY_LOGIC GetPenaltyLogicByID(int MstPenaltyLogicId)
+        {
+            return _penaltyLogicRepository.GetByID(MstPenaltyLogicId);
+        }
+
+        public List<MST_PENALTY_LOGIC> GetPenaltyLogic()
+        {
+            return _penaltyLogicRepository.Get().ToList();
+        }
+
+        public void Save(MST_PENALTY_LOGIC dbPenaltyLogic)
+        {
+            _penaltyLogicRepository.InsertOrUpdate(dbPenaltyLogic);
+            _uow.SaveChanges();
+        }
+
+    }
+}
---
+++ FMS.Website/Models/PenaltyLogicModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMS.Website.Models
+{
+    public class PenaltyLogicModel : BaseModel
+    {
+        public List<PenaltyLogicItem> Details { get; set; }
+        public PenaltyLogicModel()
+        {
+            Details = new List<PenaltyLogicItem>(); 
+        }
+    }
+    public class PenaltyLogicItem : BaseModel
+    {
+        public int MstPenaltyLogicId { get; set; }
+        public int Year { get; set; }
+        public string PenaltyLogic { get; set;}
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsActive { get; set; }
+        public SelectList KolomList { get; set; }
+        public string kolom { get; set; }
+        public SelectList OperatorList { get; set; }
+        public string Operator { get; set; }
+        public string CekRumus { get; set; }
+    }
+}
