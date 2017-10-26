61173efa4b823baa121b3d52fa5f3783394c01c1
doni <rusdani@indovto.com>
2017-10-26 12:30:17 +0700

    add files

---
+++ FMS.BusinessObject/Dto/SysAccessDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.BusinessObject.Dto
+{
+    public class SysAccessDto
+    {
+        public int MstSysAccessId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ FMS.Contract/Service/ISysAccessService.cs
@@ -0,0 +1,16 @@
+﻿using FMS.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Contract.Service
+{
+    public interface ISysAccessService
+    {
+        List<MST_SYSACCESS> GetSysAccess();
+        MST_SYSACCESS GetSysAccessById(int MstSysAccessId);
+        void Save(MST_SYSACCESS dbSysAccess);
+    }
+}
