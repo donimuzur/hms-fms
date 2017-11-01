eba50ad6df19580b32dc4c62d853205817d47722
raditya <raditya@indovti.com>
2017-11-01 17:14:23 +0700

    view and Dashboard
    layout

--- FMS.Website/Controllers/TraCrfController.cs
+++ FMS.Website/Controllers/TraCrfController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
 using FMS.Contract.BLL;
 using FMS.Core;
+using FMS.Website.Models;
 
 namespace FMS.Website.Controllers
 {
@@ -12,21 +14,39 @@ namespace FMS.Website.Controllers
     {
         //
         // GET: /TraCrf/
+        private Enums.MenuList _mainMenu;
         
         private IEpafBLL _epafBLL;
 
         public TraCrfController(IPageBLL pageBll,IEpafBLL epafBll) : base(pageBll, Core.Enums.MenuList.TraCrf)
         {
             _epafBLL = epafBll;
-            
+            _mainMenu = Enums.MenuList.TraCrf;
         }
 
         public ActionResult Index()
         {
-            return View();
+            var model = new TraCrfIndexViewModel();
+            model.MainMenu = _mainMenu;
+            
+            return View(model);
         }
 
-        
+        public ActionResult Dashboard()
+        {
+            var model = new EpafModel();
+            model.MainMenu = _mainMenu;
+            var data = _epafBLL.GetEpaf();
+            model.Details = Mapper.Map<List<EpafItem>>(data);
+            return View(model);
+        }
 
+        public ActionResult Create()
+        {
+            var model = new TraCrfItemViewModel();
+            model.MainMenu = _mainMenu;
+
+            return View(model);
+        }
     }
 }
