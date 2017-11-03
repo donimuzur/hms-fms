6ce11ef6f874078cacff4111333442be390f16bb
doni <rusdani@indovto.com>
2017-11-03 14:31:06 +0700

    add File - CtfBLL - CtfMapper - TraCtfDto, dll

---
+++ FMS.Website/Controllers/TraCtfController.cs
@@ -0,0 +1,40 @@
+﻿using FMS.Contract.BLL;
+using FMS.Core;
+using FMS.Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMS.Website.Controllers
+{
+    public class TraCtfController : BaseController
+    {
+        private IEpafBLL _epafBLL;
+        private ITraCtfBLL _ctfBLL;
+        private Enums.MenuList _mainMenu;
+        private IPageBLL _pageBLL;
+        public TraCtfController(IPageBLL pageBll, IEpafBLL epafBll, ITraCtfBLL ctfBll): base(pageBll, Core.Enums.MenuList.TraCtf)
+        {
+            _epafBLL = epafBll;
+            _ctfBLL = ctfBll;
+            _pageBLL = pageBll;
+            _mainMenu = Enums.MenuList.Transaction;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashoard()
+        {
+            var EpafData= _epafBLL.GetEpaf().Where(x => x.DocumentType == 6).ToList();
+            var model = new CtfModel();
+            foreach(var data in EpafData)
+            {
+            }
+            return View(); 
+        }
+    }
+}
---
+++ FMS.Website/Models/CtfModel.cs
@@ -0,0 +1,70 @@
+﻿using FMS.BusinessObject.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FMS.Website.Models
+{
+    public class CtfModel : BaseModel
+    {
+        public List<CtfItem> Details { get; set; }
+        public CtfModel()
+        {
+            Details = new List<CtfItem>();
+        }
+    }
+    public class CtfItem : BaseModel
+    {
+        EpafDto EPafData { get; set; }
+        public long TraCtfId { get; set; }
+        public string DocumentNumber { get; set; }
+        public int DocumentStatus { get; set; }
+        public long? EpafId { get; set; }
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string CostCenter { get; set; }
+        public int? GroupLevel { get; set; }
+        public int? Reason { get; set; }
+        public string SupplyMethod { get; set; }
+        public string PoliceNumber { get; set; }
+        public int? VehicleYear { get; set; }
+        public string VehicleType { get; set; }
+        public string VehicleUsage { get; set; }
+        public DateTime? EndRendDate { get; set; }
+        public DateTime? EffectiveDate { get; set; }
+        public bool? IsTransferToIdle { get; set; }
+        public decimal? BuyCost { get; set; }
+        public bool? ExtendVehicle { get; set; }
+        public string WithdPic { get; set; }
+        public string WithdPhone { get; set; }
+        public DateTime? WithdDate { get; set; }
+        public string WithdCity { get; set; }
+        public string WithdAddress { get; set; }
+        public decimal? EmployeeContribution { get; set; }
+        public decimal? Penalty { get; set; }
+        public decimal? RefundCost { get; set; }
+        public decimal? BuyCostTotal { get; set; }
+        public int? UserDecision { get; set; }
+        public string PenaltyPoNumber { get; set; }
+        public string PenaltyPoLine { get; set; }
+        public decimal? PenaltyPrice { get; set; }
+        public int? Remark { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsActive { get; set; }
+   
+    }
+
+    public class CtfDashboardModel : BaseModel
+    {
+       
+    }
+    public class CtfDashboardItem : BaseModel
+    {
+      
+
+    }
+}
