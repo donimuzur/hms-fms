294759689ef05cb77121da47414225e7f1b1f17f
doni <rusdani@indovto.com>
2017-11-08 13:04:01 +0700

    add create

--- FMS.Contract/BLL/ITraCtfBLL.cs
+++ FMS.Contract/BLL/ITraCtfBLL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FMS.BusinessObject.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace FMS.Contract.BLL
 {
     public interface ITraCtfBLL
     {
+        List<TraCtfDto> GetCtf();
     }
 }
