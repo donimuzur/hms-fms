7880d4a6b1a7123a17f49360536175f894bf0bb8
Yuniar Prasetyo <tyo@indovti.com>
2017-11-11 15:44:56 +0700

    add function edit and submit csf

--- FMS.DAL/Services/CsfService.cs
+++ FMS.DAL/Services/CsfService.cs
@@ -1,6 +1,8 @@
 ﻿using FMS.BusinessObject;
+using FMS.BusinessObject.Business;
 using FMS.Contract;
 using FMS.Contract.Service;
+using FMS.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,10 +29,25 @@ namespace FMS.DAL.Services
         }
 
 
-        public void save(TRA_CSF dbTraCsf)
+        public void saveCsf(TRA_CSF dbTraCsf, Login userlogin)
         {
-            _uow.GetGenericRepository<TRA_CSF>().InsertOrUpdate(dbTraCsf);
+            _uow.GetGenericRepository<TRA_CSF>().InsertOrUpdate(dbTraCsf, userlogin, Enums.MenuList.TraCsf);
             _uow.SaveChanges();
+        }
+
+        public void CancelCsf(long id, int Remark, string user)
+        {
+            var data = _csfRepository.GetByID(id);
+
+            if (data != null)
+            {
+                data.DOCUMENT_STATUS = (int)Enums.DocumentStatus.Cancelled;
+                data.MODIFIED_DATE = DateTime.Now;
+                data.MODIFIED_BY = user;
+                data.IS_ACTIVE = false;
+                data.REMARK = Remark;
+                _uow.SaveChanges();
+            }
         }
     }
 }
