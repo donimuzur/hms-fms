d06b3cb30df354c4d2a3f798cd83bbc713ab5b9a
steven <ecW5"VGK>
2017-11-14 13:30:02 +0700

    Commit Conflict

--- FMS.Website/Code/FMSWebsiteMapper.cs
+++ FMS.Website/Code/FMSWebsiteMapper.cs
@@ -126,13 +126,8 @@ namespace FMS.Website.Code
             //Begin Map Master Penalty//
             Mapper.CreateMap<PenaltyDto, PenaltyItem>().IgnoreAllNonExisting()
                 .ForMember(dest => dest.Models, opt => opt.MapFrom(src => src.Model))
-<<<<<<< HEAD
                 .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate == null ? src.CreatedDate : src.ModifiedDate))
                 .ForMember(dest => dest.ModifiedBy, opt => opt.MapFrom(src => src.ModifiedBy == null ? src.CreatedBy : src.ModifiedBy));
-=======
-                .ForMember(dest => dest.ModifiedBy, opt => opt.MapFrom(src => src.ModifiedBy == null ? src.CreatedBy : src.ModifiedBy))
-                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate == null ? src.CreatedDate : src.ModifiedDate));
->>>>>>> 8e83a66348793c2e0aacef008373996217f533a4
 
             Mapper.CreateMap<PenaltyItem, PenaltyDto>().IgnoreAllNonExisting()
                 .ForMember(dest => dest.Model, opt => opt.MapFrom(src => src.Models));
