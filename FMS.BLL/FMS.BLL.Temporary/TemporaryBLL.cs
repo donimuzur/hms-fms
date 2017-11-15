5a37f7709a267255e7e44ff81ff91a4aaed2417b
Yuniar Prasetyo <tyo@indovti.com>
2017-11-15 13:31:02 +0700

    add template for temporary

---
+++ FMS.BLL/FMS.BLL.Temporary/TemporaryBLL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Core;
+using FMS.Core.Exceptions;
+using FMS.Contract.BLL;
+using FMS.Contract.Service;
+using FMS.BusinessObject;
+using FMS.BusinessObject.Business;
+using FMS.BusinessObject.Dto;
+using FMS.BusinessObject.Inputs;
+using FMS.Contract;
+using FMS.DAL.Services;
+using FMS.Utils;
+using AutoMapper;
+using System.Data.SqlClient;
+
+namespace FMS.BLL.Temporary
+{
+    public class TemporaryBLL
+    {
+        private ITemporaryService _TemporaryService;
+        private IUnitOfWork _uow;
+
+        public TemporaryBLL(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _TemporaryService = new TemporaryService(_uow);
+        }
+    }
+}
