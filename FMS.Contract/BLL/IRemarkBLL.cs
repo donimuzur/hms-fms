8d67c8776026eef329f0da9ff4f4aa5a7cb49c5b
steven <ecW5"VGK>
2017-11-16 10:57:30 +0700

    Commit Add Change Log and Detail View Master Data

--- FMS.Contract/BLL/IRemarkBLL.cs
+++ FMS.Contract/BLL/IRemarkBLL.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FMS.BusinessObject.Business;
 
 namespace FMS.Contract.BLL
 {
@@ -12,5 +13,6 @@ namespace FMS.Contract.BLL
         List<RemarkDto> GetRemark();
         void Save(RemarkDto RemarkDto);
         RemarkDto GetRemarkById(int MstRemarkId);
+        void Save(RemarkDto dto, Login currentUser);
     }
 }
--- FMS.Contract/Service/IRemarkService.cs
+++ FMS.Contract/Service/IRemarkService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FMS.BusinessObject.Business;
 
 namespace FMS.Contract.Service
 {
@@ -14,5 +15,6 @@ namespace FMS.Contract.Service
         List<MST_REMARK> GetRemarkByDoc(int MstDocId);
         MST_REMARK GetRemarkById(int MstRemarkId);
         void save(MST_REMARK dbRemark);
+        void save(MST_REMARK dbVRemark, Login userLogin);
     }
 }
