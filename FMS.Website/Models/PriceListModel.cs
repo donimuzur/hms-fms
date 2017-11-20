7da1ef754ede24a0e308317aa7092bcebc5d96bd
steven <ecW5"VGK>
2017-11-20 22:14:57 +0700

    Commit Master Pricelist Import New Excel Template

--- FMS.Website/Models/PriceListModel.cs
+++ FMS.Website/Models/PriceListModel.cs
@@ -29,6 +29,7 @@ namespace FMS.Website.Models
         public decimal InstallmenEMP { get; set; }
         public int Vendor { get; set; }
         public string VehicleType { get; set; }
+        public string VehicleUsage { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public string ModifiedBy { get; set; }
