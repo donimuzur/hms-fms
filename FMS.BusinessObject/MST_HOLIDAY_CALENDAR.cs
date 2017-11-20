7c8d6c43539c03a77c4eb2a7e749a64ba2f015d9
raditya <raditya@indovti.com>
2017-11-20 17:11:52 +0700

    add edmx

--- FMS.BusinessObject/MST_HOLIDAY_CALENDAR.cs
+++ FMS.BusinessObject/MST_HOLIDAY_CALENDAR.cs
@@ -21,5 +21,6 @@ namespace FMS.BusinessObject
         public string MODIFIED_BY { get; set; }
         public Nullable<System.DateTime> MODIFIED_DATE { get; set; }
         public bool IS_ACTIVE { get; set; }
+        public int MST_HOLIDAY_ID { get; set; }
     }
 }
