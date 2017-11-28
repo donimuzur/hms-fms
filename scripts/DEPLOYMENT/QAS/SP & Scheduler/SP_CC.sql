7ade3ae1eda52d3b5176c6183ced17d74503a6b4
raditya <raditya@indovti.com>
2017-11-28 10:19:45 +0700

    update deployment script

--- scripts/DEPLOYMENT/QAS/SP & Scheduler/SP_CC.sql
+++ scripts/DEPLOYMENT/QAS/SP & Scheduler/SP_CC.sql
@@ -1,9 +1,9 @@
-Alter procedure dbo.GetCC
+CREATE procedure dbo.GetCC
 AS BEGIN
 
 	MERGE MST_FUNCTION_GROUP AS Target
 	USING (SELECT DISTINCT txt_acc_group, txt_cc
-	FROM dbo.View_CC) AS Source
+	FROM [HMSSQLFWOPRD.ID.PMI\PRD03].[db_Intranet_Acc_Code_P1].[dbo].[tbl_CC]) AS Source
 	ON (Source.txt_acc_group = Target.FUNCTION_NAME OR (Source.txt_acc_group IS NULL AND Target.FUNCTION_NAME IS NULL))
 	AND (Source.txt_cc = Target.COST_CENTER OR (Source.txt_cc IS NULL AND Target.COST_CENTER IS NULL))
 	WHEN NOT MATCHED BY TARGET THEN
