1fd94acd343e6edfbab72ecafbc56add01f12144
doni <rusdani@indovto.com>
2017-11-29 15:24:35 +0700

    fix email, penalty calculation, view
    update edmx mst_setting

--- FMS.BusinessObject/TRA_CTF.cs
+++ FMS.BusinessObject/TRA_CTF.cs
@@ -66,7 +66,7 @@ namespace FMS.BusinessObject
         public virtual MST_EMPLOYEE MST_EMPLOYEE { get; set; }
         public virtual MST_REMARK MST_REMARK { get; set; }
         public virtual ICollection<TRA_CTF_EXTEND> TRA_CTF_EXTEND { get; set; }
-        public virtual MST_EPAF MST_EPAF { get; set; }
         public virtual MST_REASON MST_REASON { get; set; }
+        public virtual MST_EPAF MST_EPAF { get; set; }
     }
 }
