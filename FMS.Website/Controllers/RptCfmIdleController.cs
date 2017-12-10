82e6a9053e689dbd4f38deb940b8aacafcc898fe
doni <rusdani@indovto.com>
2017-12-11 05:00:01 +0700

    add RptCfmIdleController

---
+++ FMS.Website/Controllers/RptCfmIdleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FMS.Contract.BLL;
+using FMS.Core;
+
+namespace FMS.Website.Controllers
+{
+    public class RptCfmIdleController :BaseController
+    {
+        public RptCfmIdleController(IPageBLL pageBll, Enums.MenuList menuID) : base(pageBll, menuID)
+        {
+        }
+
+        //
+        // GET: /RptCfmIdle/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
