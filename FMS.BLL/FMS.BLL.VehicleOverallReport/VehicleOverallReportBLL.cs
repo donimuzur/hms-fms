8df8074e8ec2a65c07ae1ece01fa50d82e6ab587
doni <rusdani@indovto.com>
2017-12-12 12:04:58 +0700

    add files VehicleOverallReport

---
+++ FMS.BLL/FMS.BLL.VehicleOverallReport/VehicleOverallReportBLL.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using FMS.BusinessObject.Dto;
+using FMS.BusinessObject.Inputs;
+using FMS.Contract;
+using FMS.Contract.BLL;
+using FMS.Contract.Service;
+using FMS.DAL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.BLL.VehicleOverallReport
+{
+    public class VehicleOverallReportBLL : IVehicleOverallReportBLL
+    {
+        private IVehicleOverallReportService _vehicleOverallReportService;
+        private IUnitOfWork _uow;
+
+        public VehicleOverallReportBLL(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _vehicleOverallReportService = new VehicleOverallReportService(_uow);
+        }
+
+        public List<VehicleOverallReportDto> GetVehicle(VehicleOverallReportGetByParamInput filter)
+        {
+            var data = _vehicleOverallReportService.GetVehicle(filter);
+            var redata = Mapper.Map<List<VehicleOverallReportDto>>(data);
+            return redata;
+        }
+    }
+}
---
+++ FMS.BusinessObject/Inputs/VehicleOverallReportInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.BusinessObject.Inputs
+{
+    public class VehicleOverallReportGetByParamInput
+    {
+        public bool? VehicleStatus { get; set; }
+        public string SupplyMethod { get; set; }
+        public string BodyType { get; set; }
+        public string VehicleType { get; set; }
+        public string Vendor { get; set; }
+        public string Function { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public string Regional { get; set; }
+        public string City { get; set; }
+    }
+}
