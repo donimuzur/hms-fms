22f80d94a5d42447ee3226dfd773f1f30f417b42
sidiq <sidiq@indovti.com>
2017-12-13 14:47:00 +0700

    add kpi monitoring

---
+++ FMS.DAL/LogColumn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFIS.EntitiesDAL
+{
+    public class LogColumn
+    {
+        public List<string> getColumn(string tableName)
+        {
+            List<string> columns = new List<string>();
+
+            if (tableName.Equals("MasterList"))
+            {
+                columns.Add("List Name");
+                columns.Add("List Value");
+                columns.Add("List Status");
+                columns.Add("FieldName");
+                columns.Add("FieldValue");
+                columns.Add("IsActive");
+            }
+
+            return columns;
+        }
+    }
+}
