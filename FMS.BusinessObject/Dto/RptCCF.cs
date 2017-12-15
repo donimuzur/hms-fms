104958f54c4f10bf42ffe5e322fc9569f44765b5
dona doni <doni@indovti.com>
2017-12-15 10:59:19 +0700

    report ccf

---
+++ FMS.BusinessObject/Dto/RptCCF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.BusinessObject.Dto
+{
+    public class RptCCF
+    {
+        public int Id { get; set; }
+        public string DocumentNumber { get; set; }
+        public string DocumentStatus { get; set; }
+        public string ComplaintCategory { get; set; }
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeIdComplaintFor { get; set; }
+        public string EmployeeNameComplaintFor { get; set; }
+        public string PoliceNumber { get; set; }
+    }
+}
