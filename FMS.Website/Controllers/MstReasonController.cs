b442845254281186c5d3bdd4ed6443fef26b96b9
doni <rusdani@indovto.com>
2017-12-21 16:04:34 +0700

    fix Master Complaaint validasi

--- FMS.Website/Controllers/MstReasonController.cs
+++ FMS.Website/Controllers/MstReasonController.cs
@@ -254,11 +254,24 @@ namespace FMS.Website.Controllers
                     }
                     catch (Exception exception)
                     {
-                        AddMessageInfo(exception.Message, Enums.MessageInfoType.Error);
+                        Model.MainMenu = _mainMenu;
+                        Model.CurrentLogin = CurrentUser;
+                        Model.ErrorMessage = exception.Message;
                         return View(Model);
                     }
                 }
-                _rasonBLL.SaveCanges();
+                try
+                {
+                    _rasonBLL.SaveCanges();
+                }
+                catch (Exception exp)
+                {
+                    Model.MainMenu = _mainMenu;
+                    Model.CurrentLogin = CurrentUser;
+                    Model.ErrorMessage = exp.Message;
+                    return View(Model);
+                }
+                
             }
             return RedirectToAction("Index", "MstReason");
         }
@@ -266,9 +279,6 @@ namespace FMS.Website.Controllers
         [HttpPost]
         public JsonResult UploadFile(HttpPostedFileBase upload)
         {
-            var qtyPacked = string.Empty;
-            var qty = string.Empty;
-
             var data = (new ExcelReader()).ReadExcel(upload);
             var model = new List<ReasonItem>();
             if (data != null)
