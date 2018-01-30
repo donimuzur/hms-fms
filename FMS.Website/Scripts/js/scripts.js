a2001d1de8f49bdc256070f34fcf46f68767c1dc
doni <donimuzur@gmail.com>
2018-01-30 14:35:44 +0700

    fix view all

--- FMS.Website/Scripts/js/scripts.js
+++ FMS.Website/Scripts/js/scripts.js
@@ -43,7 +43,19 @@
 		    $('.date-begin').data("DateTimePicker").maxDate(e.date);
 		});
 	    // endOf - no have future date
-		
+		$('a.action , .action-button.new a').click(function (evt) {
+		    var page_active = $('.paginate_button.current').text();
+		    var url = window.location.href;
+		    setCookie("page_active", page_active, 1);
+		    setCookie("url_active", url, 1);
+		    console.log("aaaa");
+		});
+		function setCookie(cname, cvalue, exdays) {
+		    var d = new Date();
+		    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+		    var expires = "expires=" + d.toUTCString();
+		    document.cookie = cname + "=" + cvalue + "; " + expires;
+		}
         
 		$('.datetime').datetimepicker({
 		    format: 'DD-MMM-YYYY HH:mm',
