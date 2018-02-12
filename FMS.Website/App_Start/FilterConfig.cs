2335ca0ba9b41e180a0c06f06e6f952b1e8694d8
mirza <mirza.ghulam@indovti.com>
2018-02-12 08:14:30 +0700

    Adding Global Exception Filter to catch all errors and log them to WebLogger.cs

--- FMS.Website/App_Start/FilterConfig.cs
+++ FMS.Website/App_Start/FilterConfig.cs
@@ -1,12 +1,13 @@
 ﻿using System.Web;
 using System.Web.Mvc;
-
+using FMS.Website.Filters;
 namespace FMS.Website
 {
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new UniversalErrorHandlerAttribute());
             filters.Add(new HandleErrorAttribute() {  });
         }
     }
