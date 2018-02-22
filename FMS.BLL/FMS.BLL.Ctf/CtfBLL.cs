561bcff5bfac2b99c0825951aeaaf924eff3040b
doni <donimuzur@gmail.com>
2018-02-22 13:12:06 +0700

    fix case sensitive

--- FMS.BLL/FMS.BLL.Ctf/CtfBLL.cs
+++ FMS.BLL/FMS.BLL.Ctf/CtfBLL.cs
@@ -1404,7 +1404,7 @@ namespace FMS.BLL.Ctf
                     }
                     var reason = _reasonService.GetReasonById(CtfData.REASON.HasValue ? CtfData.REASON.Value : 0);
                     var GetConfigReason = ConfigurationManager.AppSettings["Reason"].Split(',').ToList();
-                    if ((reason == null ? false : GetConfigReason.Where(x => x == reason.REASON).Count() > 0 ))
+                    if ((reason == null ? false : GetConfigReason.Where(x => (x == null ? "" : x.ToUpper() )  == reason.REASON.ToUpper()).Count() > 0 ))
                     {
                         var Employee = _employeeService.GetEmployeeById(CtfData.EMPLOYEE_ID);
                         if (Employee != null)
