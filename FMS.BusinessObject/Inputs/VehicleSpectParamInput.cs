2913413f2a4fcb2db77c4ba8d5773fe62562a34b
Steven <steven@indovti.com>
2018-03-08 16:35:01 +0700

    Commit

---
+++ FMS.BusinessObject/Inputs/VehicleSpectParamInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.BusinessObject.Inputs
+{
+    public class VehicleSpectParamInput
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Series { get; set; }
+        public string BodyType { get; set; }
+    }
+}
