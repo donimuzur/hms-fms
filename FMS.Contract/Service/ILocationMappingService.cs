ff80a8c1082b93194767f555150fece4e1b1a402
doni <donimuzur@gmail.com>
2018-03-21 10:30:46 +0700

    fix advance search mst Location mapping

--- FMS.Contract/Service/ILocationMappingService.cs
+++ FMS.Contract/Service/ILocationMappingService.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FMS.BusinessObject.Business;
+using FMS.BusinessObject.Inputs;
 
 namespace FMS.Contract.Service
 {
     public interface ILocationMappingService
     {
         List<MST_LOCATION_MAPPING> GetLocationMapping();
+        List<MST_LOCATION_MAPPING> GetLocationMapping(LocationMappingParamInput input);
         MST_LOCATION_MAPPING GetLocationMappingById(int MstLocationMappingId);
         void Save(MST_LOCATION_MAPPING dbLocationMapping);
         void Save(MST_LOCATION_MAPPING db, Login userLogin);
