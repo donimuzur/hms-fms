85c5a686e5faa453dcff35e434fb7b3d8dc0022a
doni <donimuzur@gmail.com>
2018-04-13 16:37:03 +0700

    email approval

---
+++ FMS.Website/Controllers/EmailApprovalsController.cs
@@ -0,0 +1,42 @@
+﻿using FMS.Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMS.Website.Controllers
+{
+    public class EmailApprovalsController : Controller
+    {
+        //
+        // GET: /EmailApproval/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CTF()
+        {
+            
+            var id = Request.Form["id"];
+            var email = Request.Form["email"];
+
+            var Data = new EmailApprovals();
+            try
+            {
+                Data.Status = "Success";
+                Data.Messages = string.Empty;
+            }
+            catch (Exception e)
+            {
+                Data.Status = "Failed";
+                Data.Messages = e.Message;
+            }
+
+            return Json(Data, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
