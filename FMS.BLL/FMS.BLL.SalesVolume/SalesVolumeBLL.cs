dca09dbe952c32b2660524e6404dc1da5793dd5e
doni <donimuzur@gmail.com>
2018-06-22 10:26:26 +0700

    fix show list archive data

--- FMS.BLL/FMS.BLL.SalesVolume/SalesVolumeBLL.cs
+++ FMS.BLL/FMS.BLL.SalesVolume/SalesVolumeBLL.cs
@@ -17,31 +17,61 @@ namespace FMS.BLL.SalesVolume
     public class SalesVolumeBLL : ISalesVolumeBLL
     {
         private ISalesVolumeService _SalesVolumeService;
+        private IArchSalesVolumeService _archSalesVolumeService;
         private IUnitOfWork _uow;
         public SalesVolumeBLL(IUnitOfWork uow)
         {
             _uow = uow;
             _SalesVolumeService = new SalesVolumeService(uow);
+            _archSalesVolumeService = new ArchSalesVolumeService(_uow);
         }
 
         public List<SalesVolumeDto> GetSalesVolume(SalesVolumeParamInput inputs)
         {
-            var data = _SalesVolumeService.GetSalesVolume(inputs);
-            var retData = Mapper.Map<List<SalesVolumeDto>>(data);
+            var retData = new List<SalesVolumeDto>();
+            if(inputs.Table =="2")
+            {
+                var data = _archSalesVolumeService.GetSalesVolume(inputs);
+                retData = Mapper.Map<List<SalesVolumeDto>>(data);
+            }
+            else
+            {
+                var data = _SalesVolumeService.GetSalesVolume(inputs);
+                retData = Mapper.Map<List<SalesVolumeDto>>(data);
+            }
             return retData;
         }
 
-        public List<SalesVolumeDto> GetAllSalesVolume()
+        public List<SalesVolumeDto> GetAllSalesVolume(SalesVolumeParamInput input = null)
         {
-            var data = _SalesVolumeService.GetAllSalesVolume();
-            var retData = Mapper.Map<List<SalesVolumeDto>>(data);
+            var retData = new List<SalesVolumeDto>();
+
+            if (input != null && input.Table == "2")
+            {
+                var data = _archSalesVolumeService.GetAllSalesVolume();
+                retData = Mapper.Map<List<SalesVolumeDto>>(data);
+            }
+            else
+            {
+                var data = _SalesVolumeService.GetAllSalesVolume();
+                retData = Mapper.Map<List<SalesVolumeDto>>(data);
+            }
             return retData;
         }
 
-        public SalesVolumeDto GetSalesVolumeById(int MstSalesVolumeId)
+        public SalesVolumeDto GetSalesVolumeById(int MstSalesVolumeId, bool? Archive = null)
         {
-            var data = _SalesVolumeService.GetSalesVolumeById(MstSalesVolumeId);
-            var retData = Mapper.Map<SalesVolumeDto>(data);
+            var retData = new SalesVolumeDto();
+            if (Archive.HasValue)
+            {
+                var data = _archSalesVolumeService.GetSalesVolumeById(MstSalesVolumeId);
+                retData = Mapper.Map<SalesVolumeDto>(data);
+            }
+            else
+            {
+                var data = _SalesVolumeService.GetSalesVolumeById(MstSalesVolumeId);
+                retData = Mapper.Map<SalesVolumeDto>(data);
+            }
             return retData;
         }
 
--- FMS.DAL/Services/ArchSalesVolumeService.cs
+++ FMS.DAL/Services/ArchSalesVolumeService.cs
@@ -1,11 +1,14 @@
 ﻿using FMS.BusinessObject;
 using FMS.BusinessObject.Business;
+using FMS.BusinessObject.Inputs;
 using FMS.Contract;
 using FMS.Contract.Service;
 using FMS.Core;
+using FMS.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,5 +27,48 @@ namespace FMS.DAL.Services
         {
             _uow.GetGenericRepository<ARCH_MST_SALES_VOLUME>().InsertOrUpdate(db, userlogin, Enums.MenuList.MasterData);
         }
+        public List<ARCH_MST_SALES_VOLUME> GetSalesVolume(SalesVolumeParamInput filter)
+        {
+            Expression<Func<ARCH_MST_SALES_VOLUME, bool>> queryFilter = PredicateHelper.True<ARCH_MST_SALES_VOLUME>();
+
+            if (filter != null)
+            {
+                if (filter.MonthFrom > 0)
+                {
+                    queryFilter = queryFilter.And(c => c.MONTH >= filter.MonthFrom);
+                }
+                if (filter.MonthTo > 0)
+                {
+                    queryFilter = queryFilter.And(c => c.MONTH <= filter.MonthTo);
+                }
+                if (filter.YearFrom > 0)
+                {
+                    queryFilter = queryFilter.And(c => c.YEAR >= filter.YearFrom);
+                }
+                if (filter.YearTo > 0)
+                {
+                    queryFilter = queryFilter.And(c => c.YEAR <= filter.YearTo);
+                }
+                if (!string.IsNullOrEmpty(filter.Type))
+                {
+                    queryFilter = queryFilter.And(c => c.TYPE.ToUpper() == filter.Type.ToUpper());
+                }
+                if (!string.IsNullOrEmpty(filter.Regional))
+                {
+                    queryFilter = queryFilter.And(c => c.REGION.ToUpper() == filter.Regional.ToUpper());
+                }
+            }
+
+            return _archSalesVolumeRepository.Get(queryFilter, null, "").ToList();
+        }
+        public List<ARCH_MST_SALES_VOLUME> GetAllSalesVolume()
+        {
+            return _archSalesVolumeRepository.Get().ToList();
+        }
+        public ARCH_MST_SALES_VOLUME GetSalesVolumeById(int MstSalesVolumeId)
+        {
+            return _archSalesVolumeRepository.Get().Where(x => x.MST_SALES_VOLUME_ID == MstSalesVolumeId).FirstOrDefault();
+        }
+
     }
 }
