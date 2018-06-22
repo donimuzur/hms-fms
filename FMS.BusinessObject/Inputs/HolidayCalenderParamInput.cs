fcfcc334fae972a13eabefac5686a666be81deed
Steven <steven@indovti.com>
2018-06-22 16:17:25 +0700

    Commit USER STORY#101130

--- FMS.BusinessObject/Inputs/HolidayCalenderParamInput.cs
+++ FMS.BusinessObject/Inputs/HolidayCalenderParamInput.cs
@@ -11,5 +11,6 @@ namespace FMS.BusinessObject.Inputs
         public DateTime DateFrom { get; set; }
         public DateTime DateTo { get; set; }
         public string Description { get; set; }
+        public string Table { get; set; }
     }
 }
